cbb3c76c7cdfe9f44d64ff3cc18662acc7cbc918
Angelina Latash <>
2022-05-10 17:10:06 +0200

    create vehicle service file. Add view model tests file.

---
+++ VehicleMap/VehicleList/Services/VehicleListService.swift
@@ -0,0 +1,14 @@
+//
+//  VehicleListService.swift
+//  VehicleMap
+//
+//  Created by Angelina on 10.05.22.
+//
+
+import Foundation
+
+protocol VehicleListFetching {
+}
+
+struct VehicleListService: VehicleListFetching {
+}
--- VehicleMapTests/VehicleList/ViewModels/VehicleListViewModelTests.swift
+++ VehicleMapTests/VehicleList/ViewModels/VehicleListViewModelTests.swift
@@ -1,21 +1,24 @@
 //
-//  VehicleMapTests.swift
-//  VehicleMapTests
+//  VehicleListViewModelTests.swift
+//  VehicleListViewModelTests
 //
 //  Created by Angelina on 09.05.22.
 //
 
 import XCTest
+
 @testable import VehicleMap
 
-class VehicleMapTests: XCTestCase {
+class VehicleListViewModelTests: XCTestCase {
+
+    var viewModel: VehicleListViewModel!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        viewModel = VehicleListViewModel()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        viewModel = nil
     }
 
     func testExample() throws {
@@ -25,12 +28,4 @@ class VehicleMapTests: XCTestCase {
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
